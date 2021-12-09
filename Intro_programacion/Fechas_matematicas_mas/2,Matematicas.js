9b1a92dc33e61fec9ac014a4897d17acd3177507
jhonnyer <jhonnyerg@unicauca.edu.co>
2021-12-08 22:38:38 -0500

    primer

--- Intro_programacion/Fechas_matematicas_mas/2,Matematicas.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-// Pagina operaciones y funciones matematicas revisar 
-// https://www.w3schools.com/js/js_math.asp
-console.log("Número aleatorios")
-console.log(Math.random());
-console.log(Math.random()*10);
-
-// quitar los numeros decimales 
-console.log(Math.ceil(Math.random()*10));
--- Intro_programacion/File_JS/script7_for.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var numero=100;
-var i=0;
-for(i;i<=numero;i++){
-    console.log("Numero "+i);
-    // debugger;
-}
